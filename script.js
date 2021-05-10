7dde79d5d54de7374031a293efb2aaf6f5c01a3a
Torsten Bisanz-Blank <torsten.bisanz-blank@students.coding-bootcamps.eu>
2021-05-10 15:35:51 +0200

    feat: write todos in array & write list from array

--- script.js
+++ script.js
@@ -1,36 +1,63 @@
 //buttons & EventListener
 const saveATodoButton = document.querySelector("#saveATodoButton");
 const removeDoneTodosButton = document.querySelector("#removeDoneTodosButton");
-saveATodoButton.addEventListener("click", saveTheNewTodo);
+saveATodoButton.addEventListener("click", newTodosInAnArray);
 //removeDoneTodosButton.addEventListener("click", deleteAllDoneTodos);
 
 //global variables and arry
-const stored = [];
+const storedEntrys = [];
+/**
+ * read the inputbox and write it in an array
+ */
+function newTodosInAnArray() {
+  //variables
+  let newEntry = document.querySelector("#inputBox").value;
+
+  let listObject = {};
+  listObject = {
+    text: newEntry,
+    status: "",
+  };
+  storedEntrys.push(listObject);
+  document.querySelector("#inputBox").value = "";
+  writeListFromArray();
+}
+
+/* ausgelagerte Zeilen
+
+*/
+
+/**
+ * create a button for don todos
+ */
+function createDeletemarker() {
+  let deleteMarkerElem = document.createElement("input");
+
+  deleteMarkerElem.addEventListener("change", changeFont);
+  deleteMarkerElem.type = "checkbox";
+  //deleteMarkerElem.style.marginLeft = "15px"; -->> CSS
+  deleteMarkerElem.setAttribute("class", "buttonStyle");
+
+  return deleteMarkerElem;
+}
 
 /**
  * save a new todo: add the to the list and store it in an array
  */
-function saveTheNewTodo() {
-  //variables
-  let newEntry = "";
-  newEntry = document.getElementById("#inputBox").value;
+function writeListFromArray() {
   const list = document.querySelector("#list");
   const li = document.createElement("li");
-  const deleteMarker = document.createElement("input");
 
-  deleteMarker.addEventListener("change", changeFont);
+  for (let i = 0; i < storedEntrys.length; i++) {
+    let todoText = storedEntrys[i].text;
+    const deleteMarkerElem = createDeletemarker();
 
-  //push newEntry with checkbox to list
+    li.innerText = todoText;
 
-  li.innerHTML = newEntry;
-
-  deleteMarker.type = "checkbox";
-  deleteMarker.style.marginLeft = "15px";
-  deleteMarker.setAttribute("id", "doneCheck");
-  console.log(newEntry);
-  li.appendChild(deleteMarker);
-  list.append(li);
-  //document.querySelector("#inputBox").reset();
+    li.appendChild(deleteMarkerElem);
+    list.append(li);
+    //newEntry = "";
+  }
 }
 
 /**
@@ -48,3 +75,22 @@ function changeFont(event) {
 function deleteAllDoneTodos() {
   //variables
 }
+
+/*
+push to array
+refactor: list generate from array instead with method appendchild
+write as an objekt to array as an own function
+get attribute "checked" and do as second value to the object
+  - get parent element
+  - after click "doneButton" update object in array
+  - regenerate list from array
+
+filter function
+- if questions to filter in own functions (3x)
+- get attribute "checked" from array
+- for each to scan the array
+- call function to write the list from filtered array
+
+
+
+*/
